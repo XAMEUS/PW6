ae2f9c0b98e37e7c0f490cbe47acf362888b0364
XAMEUS <xam.master.3d@gmail.com>
2016-05-08 18:43:27 +0200

    Users, register form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -26,6 +33,39 @@ class DefaultController extends Controller
         return $this->render('signin.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+        ->add('firstname', TextType::class, array('label' => 'Firstname'))
+        ->add('lastname', TextType::class, array('label' => 'Lastename'))
+        ->add('birthdate', DateTimeType::class, array('label' => 'Firstname'))
+        ->add('sex', TextType::class, array('label' => "Sex ('M' or 'F')"))
+        ->add('entry', DateTimeType::class, array('label' => 'Entry date'))
+        ->add('contract', TextType::class, array('label' => "Contract type ('CDD' or 'CDI')"))
+        ->add('duration', IntegerType::class, array('label' => "Contract duration (month)"))
+        ->add('salary', IntegerType::class, array('label' => "Salary"))
+        ->add('superior', IntegerType::class, array('label' => "Superior (id)"))
+        ->add('save', SubmitType::class, array('label' => 'Create user'))
+        ->getForm();
+
+        // tester si le formulaire est déjà rempli
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user); // prépare l'insertion dans la BD
+            $em->flush(); // insère dans la BD
+            return $this->redirectToRoute('created', array('url' => $url));
+        }
+
+        // afficher le formulaire s'il n'est pas déjà rempli
+        return $this->render('register.html.twig', array('form' => $form->createView()));
     }
 
     /**
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=15)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=30)
+     */
+    private $lastname;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthdate", type="datetime")
+     */
+    private $birthdate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sex", type="string", length=1)
+     */
+    private $sex;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="entry", type="datetime")
+     */
+    private $entry;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contract", type="string", length=3)
+     */
+    private $contract;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="duration", type="integer", nullable=true)
+     */
+    private $duration;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="salary", type="integer")
+     */
+    private $salary;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="superior", type="integer")
+     */
+    private $superior;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set birthdate
+     *
+     * @param \DateTime $birthdate
+     *
+     * @return User
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthdate
+     *
+     * @return \DateTime
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * Set sex
+     *
+     * @param string $sex
+     *
+     * @return User
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    /**
+     * Get sex
+     *
+     * @return string
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * Set entry
+     *
+     * @param \DateTime $entry
+     *
+     * @return User
+     */
+    public function setEntry($entry)
+    {
+        $this->entry = $entry;
+
+        return $this;
+    }
+
+    /**
+     * Get entry
+     *
+     * @return \DateTime
+     */
+    public function getEntry()
+    {
+        return $this->entry;
+    }
+
+    /**
+     * Set contract
+     *
+     * @param string $contract
+     *
+     * @return User
+     */
+    public function setContract($contract)
+    {
+        $this->contract = $contract;
+
+        return $this;
+    }
+
+    /**
+     * Get contract
+     *
+     * @return string
+     */
+    public function getContract()
+    {
+        return $this->contract;
+    }
+
+    /**
+     * Set duration
+     *
+     * @param integer $duration
+     *
+     * @return User
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return int
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set salary
+     *
+     * @param integer $salary
+     *
+     * @return User
+     */
+    public function setSalary($salary)
+    {
+        $this->salary = $salary;
+
+        return $this;
+    }
+
+    /**
+     * Get salary
+     *
+     * @return int
+     */
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    /**
+     * Set superior
+     *
+     * @param integer $superior
+     *
+     * @return User
+     */
+    public function setSuperior($superior)
+    {
+        $this->superior = $superior;
+
+        return $this;
+    }
+
+    /**
+     * Get superior
+     *
+     * @return int
+     */
+    public function getSuperior()
+    {
+        return $this->superior;
+    }
+}
+
