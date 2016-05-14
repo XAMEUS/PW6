5f2661b8045a889267afefe627af356b57f0516c
XAMEUS <xam.master.3d@gmail.com>
2016-05-14 13:27:50 +0200

    help/debug messages

---
+++ web/js/help.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $("#displayHelp").click(function () {
+        $("#help").slideToggle("slow");
+    });
+    $("#displayDebug").click(function () {
+        $("#debug").slideToggle("slow");
+    });
+});
