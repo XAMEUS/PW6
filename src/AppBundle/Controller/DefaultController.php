6d2b30ecfe2e44b3a27755de9e40b72edf01660d
XAMEUS <xam.master.3d@gmail.com>
2016-05-15 14:37:51 +0200

    is connected

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -42,11 +42,8 @@ class DefaultController extends Controller
       $collection = $router->getRouteCollection();
       $allRoutes = $collection->all();
 
-      $securityContext = $this->container->get('security.authorization_checker');
-
       return $this->render('home.html.twig', [
           'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-          'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED'),
       ]);
     }
 
@@ -115,7 +112,7 @@ class DefaultController extends Controller
         }
 
         // afficher le formulaire s'il n'est pas déjà rempli
-        return $this->render('register.html.twig', array('form' => $form->createView(),'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')));
+        return $this->render('register.html.twig', array('form' => $form->createView()));
     }
 
     /**
@@ -123,10 +120,8 @@ class DefaultController extends Controller
      */
     public function dashboardAction(Request $request)
     {
-        $securityContext = $this->container->get('security.authorization_checker');
         return $this->render('test/bootstrap-dashboard.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-            'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')
         ]);
     }
 
@@ -135,10 +130,8 @@ class DefaultController extends Controller
      */
     public function themeAction(Request $request)
     {
-        $securityContext = $this->container->get('security.authorization_checker');
         return $this->render('test/bootstrap-theme.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-            'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')
         ]);
     }
 
@@ -147,9 +140,8 @@ class DefaultController extends Controller
      */
     public function annuaireAction(Request $request)
     {
-        $securityContext = $this->container->get('security.authorization_checker');
         return $this->render('directory.html.twig', [
-            'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')
+
         ]);
     }
 
@@ -174,178 +166,13 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/newformation", name="newformation")
-     */
-    public function newformationAction(Request $request)
-    {
-        $formation = new Formation();
-
-        $form = $this->createFormBuilder($formation)
-        ->add('name', TextType::class, array('label' => 'Name of the formation'))
-        ->add('contents', TextareaType::class, array('label' => 'Contents', 'attr' => array('rows' => '11')))
-        ->add('date', DateType::class, array('label' => 'Date'))
-        ->add('cost', IntegerType::class, array('label' => 'Cost'))
-        ->add('duration', IntegerType::class, array('label' => 'Duration in days'))
-        ->add('prerequisites', TextareaType::class, array('label' => 'Prerequisites', 'attr' => array('rows' => '5')))
-        ->add('submit', SubmitType::class, array('label' => 'Create formation'))
-        ->getForm();
-
-        // tester si le formulaire est déjà rempli
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formation);
-            $em->flush();
-
-            return $this->redirectToRoute('formations');
-            //return $this->redirectToRoute('created', array('url' => $url));
-        }
-
-        // afficher le formulaire s'il n'est pas déjà rempli
-        return $this->render('formations/newformation.html.twig', array('form' => $form->createView()));
-    }
-
-    /**
-     * @Route("/formations", name="formations")
-     */
-    public function formationsAction(Request $request){
-        $formations = $this->getDoctrine()->getRepository("AppBundle:Formation")->findAll();
-        return $this->render("formations/formations.html.twig", ['formations' => $formations]);
-    }
-
-
-    /**
-     * @Route("/formation/{id}",name="formation", defaults={"id" = 0})
-     */
-    public function formationAction($id, Request $request){
-        $formation = $this->getDoctrine()->getRepository('AppBundle:Formation')->findOneById($id);
-
-        if(!$formation){
-            throw $this->createNotFoundException("No formation with this id : ".$id);
-        }
-
-        return $this->render("formations/formation.html.twig",['formation' => $formation]);
-    }
-
-    /**
-     * @Route("/formationapplicants",name="formationapplicants")
-     */
-
-    public function GetApplicantsAction(Request $req){
-        if($req->isXmlHttpRequest()) {
-
-            $formation = $this->getDoctrine()->getRepository("AppBundle:Formation")->findOneById($req->get('idform'));
-
-            $applicants = $formation->getApplicants();
-
-            foreach ($applicants as $a){
-                if($a->getId()==$this->getUser()->getId()){
-                    return new JsonResponse(array('data' => 1));
-                }
-            }
-            return new JsonResponse(array('data' => 0));
-        }
-        return new Response("Erreur : Ce n'est pas une requête Ajax",400);
-    }
-
-    /**
-     * @Route("/formations/joinformation",name="joinformation")
-     */
-    public function joinformationAction(Request $req){
-        if($req->isXmlHttpRequest()) {
-            $formation = $this->getDoctrine()->getRepository("AppBundle:Formation")->findOneById($req->get('idform'));
-            $formation->addApplicant($this->getUser());
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formation);
-            $em->flush();
-        }
-            return new JsonResponse(array('data' => 0));
-    }
-
-    /**
-     * @Route("/formations/addapplicant",name="formationaddapplicant")
-     */
-    public function addApplicantAction(Request $req){
-        if($req->isXmlHttpRequest()) {
-            $formation = $this->getDoctrine()->getRepository("AppBundle:Formation")->findOneById($req->get('idform'));
-            $user = $this->getDoctrine()->getRepository("AppBundle:User")->findOneById($req->get('iduser'));
-            $formation->addApplicant($user);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formation);
-            $em->flush();
-        }
-            return new JsonResponse(array('data' => 0));
-    }
-
-    /**
-     * @Route("/formations/rmapplicant",name="formationrmapplicant")
-     */
-    public function rmApplicantAction(Request $req){
-        if($req->isXmlHttpRequest()) {
-            $formation = $this->getDoctrine()->getRepository("AppBundle:Formation")->findOneById($req->get('idform'));
-            $user = $this->getDoctrine()->getRepository("AppBundle:User")->findOneById($req->get('iduser'));
-            $formation->removeApplicant($user);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formation);
-            $em->flush();
-        }
-            return new JsonResponse(array('data' => 0));
-    }
-
-    /**
-     * @Route("/formations/addintraining",name="formationaddintraining")
-     */
-    public function addIntrainingAction(Request $req){
-        if($req->isXmlHttpRequest()) {
-            $formation = $this->getDoctrine()->getRepository("AppBundle:Formation")->findOneById($req->get('idform'));
-            $user = $this->getDoctrine()->getRepository("AppBundle:User")->findOneById($req->get('iduser'));
-            $formation->addIntraining($user);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formation);
-            $em->flush();
-        }
-            return new JsonResponse(array('data' => 0));
-    }
-
-    /**
-     * @Route("/formations/rmintraining",name="formationrmintraining")
-     */
-    public function rmIntrainingAction(Request $req){
-        if($req->isXmlHttpRequest()) {
-            $formation = $this->getDoctrine()->getRepository("AppBundle:Formation")->findOneById($req->get('idform'));
-            $user = $this->getDoctrine()->getRepository("AppBundle:User")->findOneById($req->get('iduser'));
-            $formation->removeIntraining($user);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($formation);
-            $em->flush();
-        }
-            return new JsonResponse(array('data' => 0));
-    }
-
-    /**
-     * @Route("/formations/validation", name="validationformations")
-     */
-    public function validationformationsAction(Request $request){
-        $formations = $this->getDoctrine()->getRepository("AppBundle:Formation")->findByApplicantsSuperiorId($this->getUser()->getId());
-
-        $securityContext = $this->container->get('security.authorization_checker');
-
-        return $this->render("formations/validationformations.html.twig", [
-            'formations' => $formations,
-            'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')
-        ]);
-    }
-
-
-    /**
      * @Route("/calc", name="calc")
      */
     public function calcAction(Request $request)
     {
-        $securityContext = $this->container->get('security.authorization_checker');
 
         return $this->render('calc.html.twig', [
-            'connected' => $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')
+
         ]);
     }
 }
