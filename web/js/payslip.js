2630a00e25c5fe57aa53f747c631b51e7d83daf1
Quentin <quentinpn11@gmail.com>
2016-05-16 19:20:32 +0200

    fix redirection after payslip generation

--- web/js/payslip.js
+++ web/js/payslip.js
@@ -2,7 +2,23 @@ $(document).ready(function(){
     var url = window.location.href;
     var index = url.search("payslipgenerator");
     var newurl = url.substr(0, index+7)+url.substr(index+16);
+
     window.open(newurl,'window','toolbar=no, menubar=no, resizable=yes');
 
-    window.document.location = url.substr(0,index);
+    window.document.location = url.substr(0,index)+"user/"+$_GET("user");
 });
+
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace(
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;
+	}
+	return vars;
+}
